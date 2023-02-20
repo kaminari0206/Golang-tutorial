975477590918fcc0c1fcc63db039516c62086dc5
kaminari0206 <y1281820@gmail.com>
2023-02-21 05:20:46 +0900

    return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -14,17 +14,18 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("kaminari")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
 
 // Use the go mod edit command to edit the example.com/hello module to redirect Go tools
